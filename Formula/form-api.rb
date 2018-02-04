2b849c50bf80bd7089b74a932e7302462082aefc
Kamil Samigullin <kamil@samigullin.info>
2018-02-04 21:48:51 +0300

    form-api version 1.0.5

--- Formula/form-api.rb
+++ Formula/form-api.rb
@@ -1,9 +1,9 @@
 class FormApi < Formula
   desc "Data Collector as a Service."
   homepage "https://kamilsk.github.io/form-api/"
-  url "https://github.com/kamilsk/form-api/releases/download/1.0.4/form-api_1.0.4_macOS-64bit.tar.gz"
-  version "1.0.4"
-  sha256 "b2121f04702deb755aebfda32689d73df29478317bebfc40a3031f360ed47642"
+  url "https://github.com/kamilsk/form-api/releases/download/1.0.5/form-api_1.0.5_macOS-64bit.tar.gz"
+  version "1.0.5"
+  sha256 "139065acd1312cccae4a81225e2a329cf90dc95d59350d1c8b42ded5a8ce3792"
 
   def install
     bin.install "form-api"
