e7297948d853b0fd084a18a0064ab29c85d51b39
Kamil Samigullin <kamil@samigullin.info>
2018-02-19 18:21:17 +0300

    semaphore version 4.2.2

--- Formula/semaphore.rb
+++ Formula/semaphore.rb
@@ -1,9 +1,9 @@
 class Semaphore < Formula
-  desc "Semaphore pattern implementation with a timeout of lock/unlock operations based on channels."
+  desc "Semaphore pattern implementation with timeout of lock/unlock operations based on channels."
   homepage "https://github.com/kamilsk/semaphore"
-  url "https://github.com/kamilsk/semaphore/releases/download/4.2.1/semaphore_4.2.1_macOS-64bit.tar.gz"
-  version "4.2.1"
-  sha256 "e0f1ad4072a994100e1574849c840caa2b1b361a152c21150ebf3f496bad80e2"
+  url "https://github.com/kamilsk/semaphore/releases/download/4.2.2/semaphore_4.2.2_macOS-64bit.tar.gz"
+  version "4.2.2"
+  sha256 "0e598d71d65c27eab8b402ed961f7ef03d6937100729b8b2f09a96fb2855c26e"
 
   def install
     bin.install "semaphore"
