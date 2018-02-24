94f2b8a4946e06823c2dd334a479fd903140bc8f
Kamil Samigullin <kamil@samigullin.info>
2018-02-24 22:27:23 +0300

    retry version 3.0.3

--- Formula/retry.rb
+++ Formula/retry.rb
@@ -1,9 +1,9 @@
 class Retry < Formula
   desc "Functional mechanism based on channels to perform actions repetitively until successful."
   homepage "https://github.com/kamilsk/retry"
-  url "https://github.com/kamilsk/retry/releases/download/3.0.2/retry_3.0.2_macOS-64bit.tar.gz"
-  version "3.0.2"
-  sha256 "bfe09330df8ce01bd67a3966ffbb151972c5caf60c3ffda653529e9f4560b89b"
+  url "https://github.com/kamilsk/retry/releases/download/3.0.3/retry_3.0.3_macOS-64bit.tar.gz"
+  version "3.0.3"
+  sha256 "36141c12ecc2ba36e3fa5475856bf6fcc04656203709ff0e787e311c7e961b8f"
 
   def install
     bin.install "retry"
