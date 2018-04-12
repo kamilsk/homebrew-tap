31b68dc4f6612e7725889767792e88ed471850c2
Kamil Samigullin <kamil@samigullin.info>
2018-04-12 14:29:38 +0300

    Brew formula update for click version 1.2.0

--- Formula/click.rb
+++ Formula/click.rb
@@ -1,19 +1,16 @@
 class Click < Formula
   desc "Link Manager as a Service."
   homepage "https://kamilsk.github.io/click/"
-  url "https://github.com/kamilsk/click/releases/download/1.1.0/click_1.1.0_macOS-64bit.tar.gz"
-  version "1.1.0"
-  sha256 "85af4b7a08f0165fefbd44972077cae77f59329aa8621503a74ab80b23f46a77"
+  url "https://github.com/kamilsk/click/releases/download/1.2.0/click_1.2.0_macOS-64bit.tar.gz"
+  version "1.2.0"
+  sha256 "45028066f4aceab1eb7638b3325ed7d6bd84bd611792842a378a544e424f61c9"
 
   def install
     bin.install "click"
   end
 
-  def caveats
-    "Use `click --help` to see detailed help message"
-  end
-
-  test do
-    
+  def caveats; <<~EOS
+    Use `click --help` to see detailed help message
+  EOS
   end
 end
