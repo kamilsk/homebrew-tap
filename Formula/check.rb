bbf805e667a24950ce407abdf753d14301ff4d3c
Kamil Samigullin <kamil@samigullin.info>
2018-05-01 21:56:47 +0300

    Brew formula update for check version 1.0.2

--- Formula/check.rb
+++ Formula/check.rb
@@ -1,9 +1,9 @@
 class Check < Formula
   desc "Check Something as a Service."
   homepage "https://github.com/kamilsk/check"
-  url "https://github.com/kamilsk/check/releases/download/1.0.1/check_1.0.1_macOS-64bit.tar.gz"
-  version "1.0.1"
-  sha256 "9b03b79c17aa43fcc7e3549b7542c0d4e290b5c5f01065bf6abe3bebb7c61b51"
+  url "https://github.com/kamilsk/check/releases/download/1.0.2/check_1.0.2_macOS-64bit.tar.gz"
+  version "1.0.2"
+  sha256 "84d449d6611eb6d51213c866f66003fb15be1a5fb1ac3ef16642b83af07389ac"
 
   def install
     bin.install "check"
