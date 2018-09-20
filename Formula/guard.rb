b748424e883257feda4c3b17e93bc201e58e671b
Kamil Samigullin <kamil@samigullin.info>
2018-09-20 23:40:18 +0300

    Brew formula update for guard version 0.0.1

--- Formula/guard.rb
+++ Formula/guard.rb
@@ -3,10 +3,11 @@ class Guard < Formula
   homepage "https://kamilsk.github.io/guard/"
   url "https://github.com/kamilsk/guard/releases/download/0.0.1/guard_0.0.1_macOS-64bit.tar.gz"
   version "0.0.1"
-  sha256 "fbfb75a72c15f86393cbf8348271192059c550ff66040c1126a6e62c08d8f44b"
+  sha256 "8a133d999f4b7c3b736b3bd69c292a829ceb4ba042b0edb7237da783110aac61"
 
   def install
     bin.install "guard"
+    bin.install "guardctl"
   end
 
   def caveats; <<~EOS
