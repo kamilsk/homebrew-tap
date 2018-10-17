5b19d1e7ad6d3def1b6df51dee77ddda208baef7
Kamil Samigullin <kamil@samigullin.info>
2018-10-17 11:20:54 +0300

    Brew formula update for form-api version 2.2.1

--- Formula/form-api.rb
+++ Formula/form-api.rb
@@ -1,9 +1,9 @@
 class FormApi < Formula
   desc "Data Collector as a Service."
   homepage "https://kamilsk.github.io/form-api/"
-  url "https://github.com/kamilsk/form-api/releases/download/2.2.0/form-api_2.2.0_macOS-64bit.tar.gz"
-  version "2.2.0"
-  sha256 "efe8c75485d2ce95379d6fea43ea8e7f688676c1eb6d67266d5eca2dccc4efdc"
+  url "https://github.com/kamilsk/form-api/releases/download/2.2.1/form-api_2.2.1_macOS-64bit.tar.gz"
+  version "2.2.1"
+  sha256 "ce73930f129f1ad27e51df577c2ac59b66ea627bc2cd56d982dd64011c2d121c"
 
   def install
     bin.install "form-api"
