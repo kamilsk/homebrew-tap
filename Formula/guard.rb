e6818015b1e9003608fbc88b76cdc48d3379d74d
Kamil Samigullin <kamil@samigullin.info>
2018-11-24 21:54:48 +0300

    Brew formula update for guard version 1.1.0

--- Formula/guard.rb
+++ Formula/guard.rb
@@ -1,9 +1,9 @@
 class Guard < Formula
-  desc "Access Control as a Service"
+  desc "Paywall as a Service"
   homepage "https://kamilsk.github.io/guard/"
-  url "https://github.com/kamilsk/guard/releases/download/1.0.1/guard_1.0.1_macOS-64bit.tar.gz"
-  version "1.0.1"
-  sha256 "226d46fe680d1162ac0c37d8d32a7253a9d260f3e90fcd1028d608574770f596"
+  url "https://github.com/kamilsk/guard/releases/download/1.1.0/guard_1.1.0_macOS-64bit.tar.gz"
+  version "1.1.0"
+  sha256 "d333600ef477b5c063cb4b0666ee006a3dcce431c8ad6e2914847348dc672a5b"
 
   def install
     bin.install "guard"
