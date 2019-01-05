76c568ff86de312abec1521368d11b76860ca912
Kamil Samigullin <kamil@samigullin.info>
2019-01-05 09:37:49 +0300

    Brew formula update for forward version 0.2.0

--- Formula/forward.rb
+++ Formula/forward.rb
@@ -1,9 +1,9 @@
 class Forward < Formula
   desc "extended kubectl port-forward - multiple port forwarding simultaneously"
   homepage "https://github.com/kamilsk/forward"
-  url "https://github.com/kamilsk/forward/releases/download/0.1.0/forward_0.1.0_macOS-64bit.tar.gz"
-  version "0.1.0"
-  sha256 "0adb54fc09a4497432be949ea197362ad2c1cb1f184a0b75c7b93ddc9457ea12"
+  url "https://github.com/kamilsk/forward/releases/download/0.2.0/forward_0.2.0_macOS-64bit.tar.gz"
+  version "0.2.0"
+  sha256 "f191ecc5aa0afb5ddfefc6461d98c22262bdb176ea62fc2043cc1e4df9140dbf"
 
   def install
     bin.install "forward"
