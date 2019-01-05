1efac77470f7b877630775529d7300db3d787923
Kamil Samigullin <kamil@samigullin.info>
2019-01-05 19:35:34 +0300

    Brew formula update for passport version 1.1.0

--- Formula/passport.rb
+++ Formula/passport.rb
@@ -1,19 +1,16 @@
 class Passport < Formula
   desc "Person Identifier as a Service."
   homepage "https://kamilsk.github.io/passport/"
-  url "https://github.com/kamilsk/passport/releases/download/1.0.0/passport_1.0.0_macOS-64bit.tar.gz"
-  version "1.0.0"
-  sha256 "291a55c9596b4bb05a9061b24127278c8cb52120c9e718e1f287b0924c65df4c"
+  url "https://github.com/kamilsk/passport/releases/download/1.1.0/passport_1.1.0_macOS-64bit.tar.gz"
+  version "1.1.0"
+  sha256 "207ef16a22e368301f44536f4e4b5640745da2c81b765ac78bca3c280067c5ea"
 
   def install
     bin.install "passport"
   end
 
-  def caveats
-    "Use `passport --help` to see detailed help message"
-  end
-
-  test do
-    
+  def caveats; <<~EOS
+    Use `passport --help` to see detailed help message
+  EOS
   end
 end
