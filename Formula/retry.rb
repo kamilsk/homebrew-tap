77ca9d6444ede8f5c1dbd2115b0d0a7d62b4c8ea
Kamil Samigullin <kamil@samigullin.info>
2019-01-29 22:30:16 +0300

    Brew formula update for retry version v3.4.0

--- Formula/retry.rb
+++ Formula/retry.rb
@@ -1,9 +1,9 @@
 class Retry < Formula
   desc "Functional mechanism based on channels to perform actions repetitively until successful."
   homepage "https://github.com/kamilsk/retry"
-  url "https://github.com/kamilsk/retry/releases/download/3.3.1/retry_3.3.1_macOS-64bit.tar.gz"
-  version "3.3.1"
-  sha256 "0162baf5e117e09450c9aa82d3b38d6ef64ef659d5931b6283adae78f138fafd"
+  url "https://github.com/kamilsk/retry/releases/download/v3.4.0/retry_3.4.0_macOS-64bit.tar.gz"
+  version "3.4.0"
+  sha256 "7b3ee5e3926bdb3e928ccda17b04d2b719d55ab2ea24d3c37b3d3b5c2a4b3709"
 
   def install
     bin.install "retry"
