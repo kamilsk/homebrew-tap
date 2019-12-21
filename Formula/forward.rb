8020d61879f9c41eb0f54fd6b81bbb85926a362b
Kamil Samigullin <kamil@samigullin.info>
2019-12-21 21:29:43 +0300

    Brew formula update for forward version 0.5.6

--- Formula/forward.rb
+++ Formula/forward.rb
@@ -2,16 +2,16 @@
 class Forward < Formula
   desc "Extended kubectl port-forward - reliable multiple port forwarding."
   homepage "https://github.com/kamilsk/forward"
-  version "0.5.5"
+  version "0.5.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kamilsk/forward/releases/download/0.5.5/forward_0.5.5_macOS-64bit.tar.gz"
-    sha256 "22077633c4bddc28d3cb99eda1e86e0fcf46040cbf70219a381bfe0794f47e5f"
+    url "https://github.com/kamilsk/forward/releases/download/0.5.6/forward_0.5.6_macOS-64bit.tar.gz"
+    sha256 "80a94244d13f981eee6ed32445db13ef6c80a46a763da33300ed46524db94311"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kamilsk/forward/releases/download/0.5.5/forward_0.5.5_Linux-64bit.tar.gz"
-      sha256 "035d122725b8a79d4867604e60bd1b8fd4a474d76975ebe8b1ab86a9faf51da7"
+      url "https://github.com/kamilsk/forward/releases/download/0.5.6/forward_0.5.6_Linux-64bit.tar.gz"
+      sha256 "ba2697482bcbbf3cb6e4c7cd4375c42bf9b54b56e39de35568713114c6e539ba"
     end
   end
 
