3653e063cf9289a2841490dd52ebe6f53d0b943e
Kamil Samigullin <kamil@samigullin.info>
2020-02-02 15:43:33 +0300

    Brew formula update for egg version v0.0.12

--- Formula/egg.rb
+++ Formula/egg.rb
@@ -2,16 +2,16 @@
 class Egg < Formula
   desc "Extended go get - alternative for the standard `go get` with a few little but useful features."
   homepage "https://github.com/kamilsk/egg"
-  version "0.0.11"
+  version "0.0.12"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kamilsk/egg/releases/download/v0.0.11/egg_0.0.11_macOS-64bit.tar.gz"
-    sha256 "c3d0b45a8b7957373f2fe4e133adb37c85957fa16907f4275514074d6f4114de"
+    url "https://github.com/kamilsk/egg/releases/download/v0.0.12/egg_0.0.12_macOS-64bit.tar.gz"
+    sha256 "022617153f39cf2a6c8dde7d02f4cfee03990b076c501f82ba4ff4e83791e14a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kamilsk/egg/releases/download/v0.0.11/egg_0.0.11_Linux-64bit.tar.gz"
-      sha256 "0fc4232ac608b5a5be6797824b033b83d7a7ca0b701c3b59217b20f6f3010d33"
+      url "https://github.com/kamilsk/egg/releases/download/v0.0.12/egg_0.0.12_Linux-64bit.tar.gz"
+      sha256 "20cabb8910119c84229bc4e24cdfdf782e4c3a7705c426d2feeb2ec76898fd34"
     end
   end
 
